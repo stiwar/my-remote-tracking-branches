5b266a61929c8d7220e21ca0dbfcfc66170638c9
Luis Stiven <stivenartesol@gmail.com>
2024-01-29 09:57:03 -0500

    Add JS events into first paragraph

--- js/main.js
+++ js/main.js
@@ -10,3 +10,13 @@ miH1.addEventListener('mouseover', function() {
 miH1.addEventListener('mouseout', function() {
     this.style.color = 'chocolate'; // Restaura el color original cuando el mouse sale del h1
 });
+
+let primerParrafo = document.getElementById('firstParagraph');
+
+primerParrafo.addEventListener('mouseover', function() {
+    this.style.color = 'green'; // Cambia el color cuando el mouse está sobre el h1
+});
+
+primerParrafo.addEventListener('mouseout', function() {
+    this.style.color = 'red'; // Restaura el color original cuando el mouse sale del h1
+});
