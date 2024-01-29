bb93bd7553381fb55fd5932f5bb6b33356f92353
stiwar <stivenartesol@gmail.com>
2024-01-29 09:35:19 -0500

    Add mouseover and mouseout JS events

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 window.onload = function(){
     alert('Sitio cargado correctamente!');
 }
+
+const miH1 = document.querySelector('h1');
+miH1.addEventListener('mouseover', function() {
+    this.style.color = 'blue'; // Cambia el color cuando el mouse está sobre el h1
+});
+
+miH1.addEventListener('mouseout', function() {
+    this.style.color = 'chocolate'; // Restaura el color original cuando el mouse sale del h1
+});
